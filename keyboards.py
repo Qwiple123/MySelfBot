b109f03ac39c951774336907cbe1b15b9f3031b2
Qwiple <qwiple51@gmail.com>
2023-01-26 14:01:57 +0500

    Demo Bot

--- keyboards.py
+++ keyboards.py
@@ -10,6 +10,23 @@ def main_menu_reply():
         KeyboardButton(text="Стек технологий"),
         KeyboardButton(text="Чем могу быть полезен"),
         KeyboardButton(text="Контакты"),
-        KeyboardButton(text="пока незнаю...")
+        KeyboardButton(text="Мои проекты")
         )
+    return keyboard
+
+def main_keyboard():
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    keyboard.add(types.InlineKeyboardButton(text="Галерея", callback_data="gallery"),
+    types.InlineKeyboardButton(text="Стек технологий", callback_data="techno"))
+    keyboard.add(types.InlineKeyboardButton(text="Чем могу быть полезен", callback_data="impact"))
+    keyboard.add(types.InlineKeyboardButton(text="Контакты", callback_data="contact"),
+    types.InlineKeyboardButton(text="Мои проекты", callback_data="projects"))
+
+    return keyboard
+
+def back_keyboard():
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(types.InlineKeyboardButton(text="Назад", callback_data="back"))
+
+
     return keyboard
--- main.py
+++ main.py
@@ -14,29 +14,65 @@ dp = Dispatcher(bot=bot, storage=storage)
 #class Dialog(StatesGroup:
 @dp.message_handler(commands='start')
 async def start(msg: types.Message):
-    await msg.answer("Приветствую!!", reply_markup=keyboards.main_menu_reply()) 
+    await msg.answer("Привет меня зовут Максим \nЯ начинающий Python разработчик\nЯ делаю этого бота что бы попрактиковать свои навыки", reply_markup=keyboards.main_keyboard()) 
 
-@dp.message_handler(lambda message: message.text == "Галерея")
-async def gallery(msg: types.Message):
-        media = types.MediaGroup()
-        media.attach_photo(types.InputFile('gallery/pic1.JPG'), 'Превосходная фотография')
-        media.attach_photo(types.InputFile('gallery/pic2.JPG'), 'Eще одна превосходная фотография')
-        await msg.answer_media_group(media=media)
 
-@dp.message_handler(lambda message: message.text == "Стек технологий")
-async def techno(msg: types.Message):
-        await msg.answer("""Технологии которые я уже изучил \n
+@dp.callback_query_handler(text="gallery")
+async def one_of_all(query: types.CallbackQuery):
+    user_id = query.message.chat.id
+    await query.message.delete()
+    media = types.MediaGroup()
+    media.attach_photo(types.InputFile('gallery/pic1.JPG'), 'Превосходная фотография')
+    media.attach_photo(types.InputFile('gallery/pic2.JPG'), 'Eще одна превосходная фотография')
+    await bot.send_media_group(user_id, media=media)
+    await bot.send_message(user_id, 'Мои фотографии', reply_markup=keyboards.back_keyboard())
+
+
+
+
+
+
+@dp.callback_query_handler(text="techno")
+async def one_of_all(query: types.CallbackQuery):
+    user_id = query.message.chat.id
+    await query.message.delete()
+    await bot.send_message(user_id, """Технологии которые я уже изучил \n
 1. aiogram (Библиотека для написания ботов в телеграм,\n    на ней я и написал этого бота)
-2. bs4 и selenium (Библиотеки которые нужны для того что бы\n    собирать информацию с сайтов
+2. bs4 и selenium (Библиотеки которые нужны для того что бы\n    собирать информацию с сайтов)
 3. SQLite (Библиотека позволяющая создавать и использовать\n    реляционные базы данных)
 4. Базовое знание алгоритмов
-5. GIT \n\nТехнологии которые собираюсь учить \n
+5. GIT \n\n
+Технологии которые собираюсь учить \n
 1. SQL обычный язык SQL поможет мне создавать более \n    оптимизированные базы данных
 2. Docker поможет разворачивать свои приложения на \n    серверах с удобством и практичностью
 3. Alembic инструмент для миграций в базах данных
-4. Django Фреймворк для Backend-a с помощью которого \n    можно делать сайты и веб приложения """)
+4. Django Фреймворк для Backend-a с помощью которого \n    можно делать сайты и веб приложения """, reply_markup=keyboards.back_keyboard())
 
 
+@dp.callback_query_handler(text="impact")
+async def one_of_all(query: types.CallbackQuery):
+    user_id = query.message.chat.id
+    await query.message.delete()
+    await bot.send_message(user_id, "Я могу написать бота в телеграм, парсер сайта\nСоздать базу данных или автоматизировать любой процесс", reply_markup=keyboards.back_keyboard())
+
+@dp.callback_query_handler(text="contact")
+async def one_of_all(query: types.CallbackQuery):
+    user_id = query.message.chat.id
+    await query.message.delete()
+    await bot.send_message(user_id, "Telegram: https://t.me/Qwiple51\n VK: https://vk.com/qwiple_lare\n GITHUB: https://github.com/Qwiple123", reply_markup=keyboards.back_keyboard())
+
+@dp.callback_query_handler(text="projects")
+async def one_of_all(query: types.CallbackQuery):
+    user_id = query.message.chat.id
+    await query.message.delete()
+    await bot.send_message(user_id, "Пока у меня есть только один маленький pet-проект это голосовой ассистент Карен\nВот репозиторий с исходным кодом на GITHUB\nhttps://github.com/Qwiple123/Karen-Voice-Assistant", reply_markup=keyboards.back_keyboard())
+
+
+@dp.callback_query_handler(text="back")
+async def one_of_all(query: types.CallbackQuery):
+    user_id = query.message.chat.id
+    await query.message.delete()
+    await bot.send_message(user_id, "Главное меню", reply_markup=keyboards.main_keyboard())
 
 
 if __name__ == '__main__':
